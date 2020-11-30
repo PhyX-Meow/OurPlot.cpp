679aaad6c12bc25cf8c889e1460b61147c624765
MouAoZ <MouAoZ118@outlook.com>
2020-11-30 19:43:18 +0800

    头文件完善

--- include/our_plot.h
+++ include/our_plot.h
@@ -6,6 +6,10 @@
 #include <cstring>
 #include <vector>
 
+inline double abs_d(double x) {
+    return x > 0 ? x : -x;
+}
+
 using col = unsigned char;
 struct pix {
     col b;
@@ -45,25 +49,40 @@ inline point polar(double r, double th) {
 struct range {
     double min;
     double max;
+    double length() {
+        return max - min;
+    }
 };
+struct pix_pos {
+    int row;
+    int clm;
+};
+
+using img2d = std::vector<std::vector<pix>>;
+using imgrow = std::vector<pix>;
 
 class canvas_2d {
   public:
-    std::vector<std::vector<pix>> data;
-    range x;
-    range y;
-    canvas_2d(int height, int width, range x_, range y_) {
-        data = std::vector<std::vector<pix>>(height, std::vector<pix>(width, White));
-        x = x_;
-        y = y_;
+    img2d data;
+    pix &operator[](const pix_pos &pos) {
+        return data[pos.row][pos.clm];
     }
+    canvas_2d(int height, int width, range x_, range y_);
     int height() {
         return data.size();
     }
     int width() {
         return data[0].size();
     }
-    void draw_line(point ini, point end);
+    void draw_line(point ini, point end, pix color);
+
+    range x;
+    range y;
+    pix_pos origin;
+    double step_x;
+    double step_y;
+    point to_point(pix_pos pos);
+    pix_pos to_pix(point p);
 };
 class func_1var {
 };
--- src/canvas_2d.cpp
+++ src/canvas_2d.cpp
@@ -1,4 +1,30 @@
 #include "../include/our_plot.h"
+#include <string>
 
-void canvas_2d::draw_line(point ini, point end) {
+canvas_2d::canvas_2d(int height, int width, range x_, range y_) {
+    data = img2d(height, imgrow(width, White));
+    x = x_;
+    y = y_;
+    step_x = x_.length() / width;
+    step_y = y_.length() / height;
+    origin.clm = static_cast<int>(1.0 - x_.min / x_.length() * width);
+    origin.row = static_cast<int>(1.0 - y_.min / y_.length() * height);
+}
+
+pix_pos canvas_2d::to_pix(point p) {
+    return {static_cast<int>(p.y / step_y) + origin.row, static_cast<int>(p.x / step_x) + origin.clm};
+}
+point canvas_2d::to_point(pix_pos pos) {
+    return {(pos.clm - origin.clm + 0.5) * step_x, (pos.row - origin.row + 0.5) * step_y};
+}
+void canvas_2d::draw_line(point ini, point end, pix color) {
+    pix_pos start = to_pix(ini), ending = to_pix(end);
+    double slope = (end.y - ini.y) / (end.x - ini.x);
+    if (abs_d(slope) < 1) {
+        for (int i = start.clm; i <= ending.clm; ++i) {
+            double t = to_point({i, origin.row}).x;
+            double h = slope * (t - ini.x) + ini.y;
+            (*this)[to_pix({t, h})] = color;
+        }
+    }
 }
