d5e8c1b153be0fc88294d5b9fc3b8f6a52c775bf
PhyX-Meow <XueHaotian@outlook.com>
2020-12-12 16:39:08 +0800

    to_affine

--- src/canvas_2d.cpp
+++ src/canvas_2d.cpp
@@ -10,28 +10,19 @@ canvas_2d::canvas_2d(int height, int width, range x_, range y_) {
     origin.clm = i_floor(-x_.min / x_.length() * width);
     origin.row = i_floor(-y_.min / y_.length() * height);
 }
-point canvas_2d::aff_trans(point p) { //need repair
+point canvas_2d::to_affine(point p) {
     return {p.x / step_x, p.y / step_y};
 }
-pix_pos canvas_2d::to_pix_noaff(point p) {
-    return {i_floor(p.x) + origin.clm, i_floor(p.y) + origin.row};
+point canvas_2d::to_affine(pix_pos pos) {
+    return {pos.clm - origin.clm + 0.5, pos.row - origin.row + 0.5};
 }
 pix_pos canvas_2d::to_pix(point p) {
-    return {i_floor(p.x / step_x) + origin.clm, i_floor(p.y / step_y) + origin.row};
+    return {i_floor(p.x) + origin.clm, i_floor(p.y) + origin.row};
 }
-point canvas_2d::to_point(pix_pos pos) {
-    return {(pos.clm - origin.clm + 0.5) * step_x, (pos.row - origin.row + 0.5) * step_y};
-}
-void canvas_2d::draw_line(point ini, point end, pix color) {
-    //first step: affine trans.
-    point start{aff_trans(ini)};
-    point ending{aff_trans(end)};
 
-    //second step: search responding pixel positions
+void canvas_2d::draw_line(point ini_, point end_, pix color) {
+    affine ini = to_affine(ini_), end = to_affine(end_);
 
-    //last step: for every pixel position, paint 9*9 square
-
-    /*
     pix_pos start = to_pix(ini), ending = to_pix(end);
     double slope = (end.y - ini.y) / (end.x - ini.x);
     double c;
@@ -43,14 +34,13 @@ void canvas_2d::draw_line(point ini, point end, pix color) {
         if (ini.x > end.x)
             std::swap(ini, end), std::swap(start, ending);
         for (int i = start.clm; i <= ending.clm; ++i) {
-            double t = to_point({i, origin.row}).x;
+            double t = to_affine(pix_pos{i, origin.row}).x;
             double h = slope * (t - ini.x) + ini.y;
             centre = to_pix({t, h});
             if (contains(centre))
                 (*this)[centre] = color;
             for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
-                    //dist = (to_point({centre.clm + i, centre.row + j}).x * slope + c - to_point({centre.clm + i, centre.row + j}).y) * (to_point({centre.clm + i, centre.row + j}).x * slope + c - to_point({centre.clm + i, centre.row + j}).y) / (slope * slope + 1);
                 }
             }
         }
@@ -60,12 +50,12 @@ void canvas_2d::draw_line(point ini, point end, pix color) {
         if (ini.y > end.y)
             std::swap(ini, end), std::swap(start, ending);
         for (int i = start.row; i <= ending.row; ++i) {
-            double t = to_point({i, origin.clm}).y;
+            double t = to_affine(pix_pos{i, origin.clm}).y;
             double h = slope * (t - ini.y) + ini.x;
             if (contains(to_pix({h, t})))
                 (*this)[to_pix({h, t})] = color;
         }
-    }*/
+    }
 }
 canvas_2d &operator<<(canvas_2d &target, func_1var shape) {
     shape.paint_to(target);
