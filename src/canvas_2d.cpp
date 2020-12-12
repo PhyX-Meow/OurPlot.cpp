f4f0a52332503d6e678477af1a8bf750bbbf29e7
MouAoZ <MouAoZ118@outlook.com>
2020-12-12 16:41:28 +0800

    ////

--- src/canvas_2d.cpp
+++ src/canvas_2d.cpp
@@ -22,22 +22,17 @@ pix_pos canvas_2d::to_pix(point p) {
 point canvas_2d::to_point(pix_pos pos) {
     return {(pos.clm - origin.clm + 0.5) * step_x, (pos.row - origin.row + 0.5) * step_y};
 }
-void canvas_2d::draw_line(point ini, point end, pix color) {
+void canvas_2d::draw_line(point ini_, point end_, pix color) {
     //first step: affine trans.
-    point start{aff_trans(ini)};
-    point ending{aff_trans(end)};
 
-    //second step: search responding pixel positions
-
-    //last step: for every pixel position, paint 9*9 square
-
-    /*
-    pix_pos start = to_pix(ini), ending = to_pix(end);
-    double slope = (end.y - ini.y) / (end.x - ini.x);
-    double c;
+    point ini{to_affine(ini_)};
+    point end{to_affine(end_)};
+    pix_pos start{to_pix(ini)};
+    pix_pos ending{to_pix(end)};
+    double slope, c, dist;
+    slope = (end.y - ini.y) / (end.x - ini.x);
     pix_pos centre;
-    double dist;
-
+    //second step: search responding pixel positions
     if (abs_d(slope) <= 1.0) {
         c = end.y - slope * end.x;
         if (ini.x > end.x)
@@ -46,11 +41,14 @@ void canvas_2d::draw_line(point ini, point end, pix color) {
             double t = to_point({i, origin.row}).x;
             double h = slope * (t - ini.x) + ini.y;
             centre = to_pix({t, h});
-            if (contains(centre))
-                (*this)[centre] = color;
             for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
-                    //dist = (to_point({centre.clm + i, centre.row + j}).x * slope + c - to_point({centre.clm + i, centre.row + j}).y) * (to_point({centre.clm + i, centre.row + j}).x * slope + c - to_point({centre.clm + i, centre.row + j}).y) / (slope * slope + 1);
+                    if (i != 0 && j != 0)
+                        dist = (to_point({centre.clm + i, centre.row + j}).x * slope + c - to_point({centre.clm + i, centre.row + j}).y) * (to_point({centre.clm + i, centre.row + j}).x * slope + c - to_point({centre.clm + i, centre.row + j}).y) / (slope * slope + 1);
+                    else
+                        dist = 0;
+                    if (contains({centre.clm + i, centre.row + j}))
+                        (*this)[{centre.clm + i, centre.row + j}] = color << dist;
                 }
             }
         }
@@ -62,55 +60,65 @@ void canvas_2d::draw_line(point ini, point end, pix color) {
         for (int i = start.row; i <= ending.row; ++i) {
             double t = to_point({i, origin.clm}).y;
             double h = slope * (t - ini.y) + ini.x;
-            if (contains(to_pix({h, t})))
-                (*this)[to_pix({h, t})] = color;
+            centre = to_pix({h, t});
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    if (i != 0 && j != 0)
+                        dist = (to_point({centre.clm + i, centre.row + j}).x * slope + c - to_point({centre.clm + i, centre.row + j}).y) * (to_point({centre.clm + i, centre.row + j}).x * slope + c - to_point({centre.clm + i, centre.row + j}).y) / (slope * slope + 1);
+                    else
+                        dist = 0;
+                    if (contains({centre.clm + i, centre.row + j}))
+                        (*this)[centre.clm + i, centre.row + j] = color << dist;
+                }
+            }
         }
-    }*/
-}
-canvas_2d &operator<<(canvas_2d &target, func_1var shape) {
-    shape.paint_to(target);
-    return target;
-}
+        //last step: for every pixel position, paint 9*9 square
+    }
+    canvas_2d &operator<<(canvas_2d &target, func_1var shape) {
+        shape.paint_to(target);
+        return target;
+    }
 
-void func_1var::paint_to(canvas_2d target) {
-    pix color = get_color();
-    double precis = get_precis();
-    point ini{0, func(0)}, end{precis, func(precis)};
-    while (target.contains(target.to_pix(end))) {
-        target.draw_line(ini, end, color);
-        ini = end;
-        end.x += precis;
-        end.y = func(end.x);
+    void func_1var::paint_to(canvas_2d target) {
+        pix color = get_color();
+        double precis = get_precis();
+        point ini{0, func(0)}, end{precis, func(precis)};
+        while (target.contains(target.to_pix(end))) {
+            target.draw_line(ini, end, color);
+            ini = end;
+            end.x += precis;
+            end.y = func(end.x);
+        }
+        ini = {0, func(0)};
+        end = {-precis, func(-precis)};
+        while (target.contains(target.to_pix(end))) {
+            target.draw_line(ini, end, color);
+            ini = end;
+            end.x -= precis;
+            end.y = func(end.x);
+        }
     }
-    ini = {0, func(0)};
-    end = {-precis, func(-precis)};
-    while (target.contains(target.to_pix(end))) {
-        target.draw_line(ini, end, color);
-        ini = end;
-        end.x -= precis;
-        end.y = func(end.x);
-    }
-}
-void func_para::paint_to(canvas_2d target) {
-    double para{precis};
-    int cnt{0};
-    point ini{func_x(0), func_y(0)}, end{func_x(precis), func_y(precis)};
-    while (target.contains(target.to_pix(end)) && cnt <= end_cnt) {
-        target.draw_line(ini, end, color);
-        ini = end;
-        para += precis;
-        end = {func_x(para), func_y(para)};
-        cnt++;
-    }
-    ini = {func_x(0), func_y(0)};
-    end = {func_x(-precis), func_y(-precis)};
-    para = -precis;
-    cnt = 0;
-    while (target.contains(target.to_pix(end)) && cnt <= end_cnt) {
-        target.draw_line(ini, end, color);
-        ini = end;
-        para -= precis;
-        end = {func_x(para), func_y(para)};
-        cnt++;
+    void func_para::paint_to(canvas_2d target) {
+        double para{precis};
+        int cnt{0};
+        point ini{func_x(0), func_y(0)}, end{func_x(precis), func_y(precis)};
+        while (target.contains(target.to_pix(end)) && cnt <= end_cnt) {
+            target.draw_line(ini, end, color);
+            ini = end;
+            para += precis;
+            end = {func_x(para), func_y(para)};
+            cnt++;
+        }
+        ini = {func_x(0), func_y(0)};
+        end = {func_x(-precis), func_y(-precis)};
+        para = -precis;
+        cnt = 0;
+        while (target.contains(target.to_pix(end)) && cnt <= end_cnt) {
+            target.draw_line(ini, end, color);
+            ini = end;
+            para -= precis;
+            end = {func_x(para), func_y(para)};
+            cnt++;
+        }
     }
 }
