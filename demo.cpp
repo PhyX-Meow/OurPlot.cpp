2d59b2b18e723a3caeb5453ef397c9f120a8d460
PhyX-Meow <XueHaotian@outlook.com>
2021-01-01 13:56:21 +0800

    Add ueu's number

--- demo.cpp
+++ demo.cpp
@@ -1,14 +1,11 @@
 #include "./include/our_plot.h"
+#include "include/plot_2d.h"
 
 int main() {
-    canvas_2d c(1000, 1000, {-1.5, 1.5}, {-1.5, 1.5});
-    func_1var f([](double x) { return asin(std::abs(x) - 1) - pi / 2; }, Red, thin, 0.01);
-    func_polar heart([](double t) { return 1 - sin(t); }, {0, 2 * pi}, Red, thin);
-    func_polar flower([](double t) { return sin(1.3 * t); }, {0, 20 * pi}, Red, thick, 0.01);
-    func_para circle([](double x) { return point{cos(x), sin(x)}; }, {0, 2 * pi}, Black, thin);
-    c.draw_axes();
-    c << flower;
-    c.save_as("demo.bmp");
+    canvas_2d task_1(1000, 1000, {-8, 8}, {-8, 8});
+    task_1.draw_axes();
+    task_1 << line({-10, -10}, {10, 10}, pix(0x246224), medium);
+    task_1.save_as("task_1.bmp");
 
     return 0;
 }
